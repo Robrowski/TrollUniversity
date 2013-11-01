e0d11796dd0b18d89adfc075eeac62edefd17671
Robert Dabrowski <rpdabrowski@wpi.edu>
2013-11-01 02:48:02 -0400

    email hooks use basic version of remove_tags

--- TrollUniversity/src/main_page.py
+++ TrollUniversity/src/main_page.py
@@ -7,18 +7,28 @@ from models import Message
 
 import json
 import logging
+from utils.message.processing import remove_tags as tagRemover
 
 class WebhookHandler(webapp.RequestHandler):
     def post(self):
         message = json.loads(self.request.body)
         logging.error(message['message_data']['body'][0]['content'])
+       
         subject = message['message_data']['subject']
+        subject = tagRemover.remove_tags(subject)  # Remove the tags
+
+        
         rawBody = message['message_data']['body'][0]['content']
+        rawBody = tagRemover.remove_tags(rawBody)  # Remove the tags
+        
         dbMessage = Message(subject = subject,
                             body = rawBody)
         
         #process
-        # - normalize the email - removings tags
+        # - normalize the email - 
+        # removings tags
+        
+        
         # - check for duplicates
         
         dbMessage.put()
